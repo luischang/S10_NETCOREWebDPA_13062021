ac2db0a6f617f2e6ae026fcc715e7946c4c2bf6a
Luis Chang <luis.chang@outlook.com>
2021-06-13 12:50:46 -0500

    Agregar archivos de proyecto.

---
+++ S10_NETCOREWebDPA.API/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using S10_NETCoreWebDPA.Domain.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S10_NETCOREWebDPA.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+
+        [HttpGet]
+        [Route("GetCustomer")]
+        public async Task<IActionResult> GetCustomer()
+        {
+            var customers = await _customerRepository.GetCustomers();
+            return Ok(customers);          
+
+        }
+
+
+
+
+    }
+}
---
+++ S10_NETCoreWebDPA.Domain/Core/Entities/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace S10_NETCoreWebDPA.Domain.Core.Entities
+{
+    public partial class Users
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+        public bool? Status { get; set; }
+    }
+}
